32c4c9cc47bceb8310a5b0b5c4dda9999b68decb
ZouariAhmedNour <zouariahmednour@gmail.com>
2025-11-18 11:23:38 +0100

    Ajout initial des dossiers backend et frontend avec structure complète

---
+++ backend/neverAlone/src/main/groovy/com/signloop/neveralone/NeverAloneApplication.groovy
@@ -0,0 +1,13 @@
+package com.signloop.neveralone
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NeverAloneApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NeverAloneApplication, args)
+	}
+
+}
---
+++ backend/neverAlone/src/test/groovy/com/signloop/neveralone/NeverAloneApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.signloop.neveralone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeverAloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
